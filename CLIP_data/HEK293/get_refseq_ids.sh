42f92871bdc65753dcd45ac537df03240e1f36e1
Felix Wu <felix.l.wu88@gmail.com>
2017-04-16 16:47:25 -0400

    wrote a quick bedtools intersect script that finds the refseq ids that overlap a given set of training data

---
+++ CLIP_data/HEK293/get_refseq_ids.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+ago_bed="$1"
+chr="chr22"
+ref_bed="../../ref/refseq.hg19.${chr}.tx.bed"
+
+bedtools intersect -a ${ref_bed} -b ${ago_bed} | cut -f 4 > ${ago_bed%.*}.refseq.${chr}.txt
+#bedtools intersect -a ${ref_bed} -b ${ago_bed} | cut -f 4 > .tmp.int
+
+# cut -f 4 ${ref_bed} > .tmp.all
+
+# grep -vF -f .tmp.int .tmp.all > 
